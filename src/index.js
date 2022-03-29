6c56dc289534dab0141c286d93d02058a00fe47a
Darrian Bagley <dbagle14@gmail.com>
2022-03-28 22:11:01 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
+// Quokka Config
+({
+    plugins: ["jsdom"],
+    "jsdom": {
+        "file": "/home/dbagle7/Development/Code/Assignments/phase-1/phase-1-js-fetch-on-demand-with-forms/index.html",
+    }
+});
+
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('#search-form');
+    const input = document.querySelector('#searchByID');
+
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let value = input.value;
+
+        fetch(`http://localhost:3000/movies/${value}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const title = document.querySelector('#title');
+                const summary = document.querySelector('#summary');
+
+                title.innerHTML = data.title;
+                summary.innerHTML = data.summary;
+            });
+    });
+};
 
 document.addEventListener('DOMContentLoaded', init);
